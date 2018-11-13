75a4cd815fdb2d79eeb47a8c15a36af19dc832ce
Michael Plesser <plesser.michael@gmail.com>
2018-11-13 14:59:47 +0100

    fresh version of H4Analysis, due to make errors

--- rootlogon.C
+++
@@ -1,5 +0,0 @@
-{
-    gStyle->SetPalette(kDeepSea);
-    gSystem->Load("./CfgManager/lib/libCFGMan.so");
-    gSystem->Load("./lib/libH4Analysis.so");
-}
--- scripts/rootlogon.C
+++
@@ -1,5 +0,0 @@
-{
-    gSystem->Load("CfgManager/lib/CfgManagerDict.so");
-    gSystem->Load("lib/H4Dict.so");
-    gStyle->SetPalette(51);
-}
